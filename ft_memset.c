dfedb1ae572d018a680333fc0759e7937a381140
5thwin <hisy2059@naver.com>
2021-01-19 20:58:16 +0900

    memset, libft.a

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunoh <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 17:23:24 by seunoh            #+#    #+#             */
+/*   Updated: 2021/01/19 17:23:24 by seunoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	unsigned char	*dest;
+	unsigned char	fill;
+	dest = s;
+	fill = c;
+	i = 0;
+	while (i++ < n)
+	{
+		*(dest + i) = fill;
+	}
+	return (s);
+}
