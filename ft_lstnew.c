a309818c5972dcfca79b840901e7e1277ebcec5c
5thwin <hisy2059@naver.com>
2021-02-17 16:39:07 +0900

    chech norm

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: seunoh <seunoh@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:26:45 by seunoh            #+#    #+#             */
-/*   Updated: 2021/02/17 14:26:45 by seunoh           ###   ########.fr       */
+/*   Updated: 2021/02/17 16:30:34 by seunoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*elem;
 
-	if (!(elem = (t_list *)malloc(sizeof(t_list)))
+	if (!(elem = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
 	elem->content = content;
 	elem->next = NULL;
